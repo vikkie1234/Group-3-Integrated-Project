f16e4e2305df672a735471032d144580ef6bac91
Zhilong-coding <zhilong.k.liang@gmail.com>
2021-10-27 11:25:16 +0200

    Add Login

--- Integrated-Project/Controllers/HomeController.cs
+++ Integrated-Project/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using Integrated_Project.Model;
 
 namespace Integrated_Project.Controllers
 {
@@ -32,6 +30,30 @@ namespace Integrated_Project.Controllers
             ViewBag.Message = "Register page.";
 
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(TblDetail objUser)
+        {
+            //string constr = @"Data Source=localhost;Initial Catalog=UserDetailsdb;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework";
+            //SqlConnection cn = new SqlConnection(constr);
+            //string query = "select"
+            return Redirect("https://techspo.co/about/");
+
+            if (ModelState.IsValid)
+            {
+                using (UserDetailsdbEntities db = new UserDetailsdbEntities())
+                {
+                    var obj = db.TblDetails.Where(a => a.Name.Equals(objUser.Name)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.id.ToString();
+                        Session["UserName"] = obj.Name.ToString();
+                        return Redirect("https://techspo.co/about/");
+                    }
+                }
+            }
+            return View(objUser);
         }
     }
 }
